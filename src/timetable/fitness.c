b73ea60efa14c991024f50d93c97f4a47d38abfd
HitenSachani <146418778+HitenSachani@users.noreply.github.com>
2024-01-08 14:38:21 +0530

    fitness.c

--- src/timetable/fitness.c
+++ src/timetable/fitness.c
@@ -2,9 +2,20 @@
 
 static uint computeHardConstraint_PartyNonCollision(Population *population, TimeTableSpecifications, uint timetable_index){
   	uint n = 0;
-	for(uint i = 0; i < population->size; i++){
-		//getTimeTableTuple();
-	}
+		for(uint i = 0; i < population->tt_size; i++){
+        		uint *venue_id_i;
+        		uint *timeslot_id_i;
+			getTimeTableTuple(population, timetable_index, i, venue_id_i, timeslot_id_i);
+        		for(uint j = i+1; j < population->size; j++){
+            			uint *venue_id_j;
+            			uint *timeslot_id_j;
+            			getTimeTableTuple(population, timetable_index, j, venue_id_j, timeslot_id_j);
+            			if (timeslot_id_i == timeslot_id_j && venue_id_i == venue_id_j){
+                			n++;
+            			}
+        		}
+    		}
+    	return n;
 }
 
 
@@ -16,9 +27,9 @@ static numeric computeHardFitness(Population *population, TimeTableSpecification
 
 void computeFitnesses(Population *population, TimeTableSpecifications *specfications, numeric *soft_fitness, numeric *hard_fitness) {
 
-	// Iterate through all members of the population
+	Iterate through all members of the population
 	for (uint i = 0; i < population->size; i++) {
-		//soft_fitness[i] = computeSoftFitness(population, i);
-		//hard_fitness[i] = computeHardFitness(population, i);
+		soft_fitness[i] = computeSoftFitness(population, i);
+		hard_fitness[i] = computeHardFitness(population, i);
 	}
 }
