ead63f0949c5e54a971e615e88debed405aaa3f5
HitenSachani <146418778+HitenSachani@users.noreply.github.com>
2024-01-08 13:55:15 +0530

    fitness.c

--- src/timetable/fitness.c
+++ src/timetable/fitness.c
@@ -1,1 +1,18 @@
 #include "fitness.h"
+
+static uint computeHardConstraint_PartyNonCollision(Population *population, TimeTableSpecifications, uint timetable_index){
+  
+}
+
+static numeric computeSoftFitness(Population *population, uint timetable_index){
+}
+
+static numeric computeSoftFitness(Population *population, uint timetable_index){
+}
+
+void computeFitness(Population *population, numeric soft_fitness, numeric *hard_fitness){
+  for (uint i = 0; i < population->size; i++){
+    soft->fitness[i] = computeSoftFitness(population, i);
+    hard_fitness[i] =  computeHardFitness(population, i);
+  }
+}
