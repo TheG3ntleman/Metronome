c50df96bf3e705d9f0e713b098636719ec08fc18
HitenSachani <146418778+HitenSachani@users.noreply.github.com>
2024-01-09 13:49:41 +0530

    fitness.c

--- src/timetable/fitness.c
+++ src/timetable/fitness.c
@@ -5,41 +5,73 @@
 #ifdef HARD_CAPACITY_CONSTRAINT
 static numeric computeHardConstraint_CapacityConstraint(Population *population, TimeTableSpecifications *specifications, uint timetable_index) {
 	
-	// Running through all the sessions.
-	
-	numeric violations = 0;
+    	// creating an array of array which conatains all the session and parties associated with them
+    	uint session_party[population->n_sessions][10];
+    	for (uint i = 0; i < population->n_sessions; ++i) {
+        	for (uint j = 0; j < 10; ++j) {
+            		session_party[i][j] = -1;
+        	}
+    	}
 
-	for (uint i = 0; i < population->n_sessions; i++) {
-		
-		// Retrieving venue capacity.
+     	for (uint i = 0; i < population->n_sessions; i++) {
+		session_party[i][0] = i
+		uint n = 0;
+		for (uint j = 0; j < specifications->assignments->size, j++) {
+	    		if (i == specifications->assignments->session_id[j]) {
+				session_party[i][n] = specifications->assignments->party_id[j]
+				n++;
+	    		}
+		}
+    	}       
+
+    	// converting from partyid to party strenght in session_party array
+    	for (uint i = 1; i < 10; i++) {
+        	for (uint j = 0; j < population->n_sessions; j++) {
+            		for (uint k = 0; k < specifications->parties->size; k++){
+                		if (specifications->parties->party_id[k] == session_party[j][i]) {
+                    			session_party[j][i] = specifications->parties->strenght[k]
+                		}
+            		}
+        	}
+    	}
+
+    	// creating an array of array which contains all the sessions and the strenghts associated with them
+    	uint session_strenght[population->n_sessions][2];
+    	for (uint i = 0; i < population->n_sessions; ++i) {
+        	for (uint j = 0; j < 2; ++j) {
+            	session_strenght[i][j] = -1;
+        	}
+    	}
+
+    	for (uint i = 0; i < population->sessions; i++) {
+        	session_strenght[i][0] = session_party[i][0]
+        	uint n = 0;
+        	for (uint j = 1; j < 10; j++) {
+            		if (session_party[i][j] != -1) {
+                	n += session_party[i][j]
+            		}
+        	}
+        	session_strenght[i][1] = n
+    	}
+
+    	numeric violations = 0;
+
+    	//comapring strenght for each session with the capacity of the venue its scheduled at
+    	for (uint i = 0; i < population->n_sessions; i++) {
 		uint timeslot_id;
 		uint venue_id;
 		getTimeTableTuple(population, timetable_index, i, &venue_id, &timeslot_id);
+        	for (uint j = 0; j < population->n_sessions; j++) {
+            		if (session_strenght[j][0] == i) {
+                		// Final incrementation
+                		if (session_strength[j][1] > specifications->venues->capacity[venue_id]) {
+                    			violations++;
+                		}
+            		}
+        	}
+    	}
 
-		// Computing sessions strength
-		uint strength = 0;
-		for (uint j = 0; j < specifications->assignments->size; j++) {
-			// Running through all the parties
-			if (specifications->assignments->session_id[j] == i) {
-				uint party_id = specifications->assignments->party_id[j];
-				// Finding corresponding party strength and incrementing it
-				for (uint k = 0; k < specifications->parties->size; k++) {
-					if (specifications->parties->party_id[k] == party_id) { 
-						strength += specifications->parties->strength[k];
-						break;
-					}
-				}
-			}
-		} 
-
-		// Final incrementation
-		if (strength > specifications->venues->capacity[venue_id]) {
-			violations++;
-		}
-
-	}
-
-	return violations;
+    	return violations;
 
 }
 #endif
