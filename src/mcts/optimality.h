f2e36b816002dbd80f946b0bb5f908f5fc390cf8
HitenSachani <146418778+HitenSachani@users.noreply.github.com>
2024-03-24 20:33:06 +0530

    optimality.h

--- src/mcts/optimality.h
+++ src/mcts/optimality.h
@@ -5,7 +5,7 @@
 #include "agent.h"
 #include "state_space_tree.h"
 
-snumeric get_optimality(TimeTableEntry *timetable,
+snumeric get_optimality(TimeTableEntry *timetable, uint depth,
                       TimeTableSpecifications *specifications);
 
 #endif
