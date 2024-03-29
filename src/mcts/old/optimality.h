e317cba83dab0d418b2ce39ed30c6ea1c4a5f268
HitenSachani <146418778+HitenSachani@users.noreply.github.com>
2024-03-29 15:56:10 +0530

    Updated optimality.h to be compatible with normalization

--- src/mcts/old/optimality.h
+++ src/mcts/old/optimality.h
@@ -8,4 +8,41 @@
 snumeric get_optimality(TimeTableEntry *timetable, uint depth,
                       TimeTableSpecifications *specifications);
 
+// writing all soft constraint fitness functions so that they can be called in normalization.h
+numeric soft_constraint_student_travel_time(TimeTableEntry *timetable,
+                                            uint n_sessions, uint depth,
+                                            TimeTableSpecifications *specs);
+
+numeric soft_constraint_maximize_chunking(TimeTableEntry *timetable,
+                                          uint n_sessions, uint depth,
+                                          TimeTableSpecifications *specs);
+
+numeric soft_constraint_room_utlisation(TimeTableEntry *timetable,
+                                        uint n_sessions, uint depth,
+                                        TimeTableSpecifications *specs);
+
+numeric soft_constraint_avoid_early_time(TimeTableEntry *timetable,
+                                         uint n_sessions, uint depth,
+                                         TimeTableSpecifications *specs);
+
+numeric soft_constraint_room_capacity_utilisation(TimeTableEntry *timetable,
+                                          uint n_sessions, uint depth,
+                                          TimeTableSpecifications *specs);
+
+numeric soft_constraint_common_timeslot_empty(TimeTableEntry *timetable,
+                                              uint n_sessions, uint depth,
+                                              TimeTableSpecifications *specs);
+
+numeric soft_constraint_minimize_backtoback_teacher_classes(TimeTableEntry *timetable,
+                                              uint n_sessions, uint depth,
+                                              TimeTableSpecifications *specs);
+
+numeric soft_constraint_minimize_samecourse_sessions(TimeTableEntry *timetable,
+                                             uint n_sessions, uint depth,
+                                             TimeTableSpecifications *specs);
+
+numeric soft_constraint_evenly_throughout_week(TimeTableEntry *timetable,
+                                              uint n_sessions, uint depth,
+                                              TimeTableSpecifications *specs);
+
 #endif
