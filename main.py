e4561b85a455fd6be12d57f09a644692fbc4a401
HitenSachani <146418778+HitenSachani@users.noreply.github.com>
2024-04-19 23:05:15 +0530

    added manual problem in main.py

--- main.py
+++ main.py
@@ -13,3 +13,72 @@ scaler_genetic_optimizer.optimize()
 
 # Plotting minimum violations and average violations in two seperate plots
 scaler_genetic_optimizer.plot_violations()
+
+manual_party_strength = [70, 18, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+manual_party_names = ["ECM", "CM", "Gopinath", "Pooran", "Ramakant", "veeraiya", "Jayaprakash", "ravibabu", "Mahesh", "Praveen", "Pankaj", "manvi"]
+
+
+# Set party data
+for i in range(sample_time_table_specifications.number_of_parties):
+    sample_time_table_specifications.party_ids.append(i)
+    sample_time_table_specifications.party_strengths.append(manual_party_strength[i])
+    sample_time_table_specifications.party_max_hours.append(2)
+    sample_time_table_specifications.party_type.append(1)
+    sample_time_table_specifications.party_preffered_start_time.append(1)  # 9:30
+    sample_time_table_specifications.party_preffered_end_time.append(6)  # 4:30
+    sample_time_table_specifications.party_preffered_back_to_back.append(2)  # Maximum 2 sessions back to back
+    sample_time_table_specifications.party_preffered_max_hours.append(6)  # Prefer conferences to last for 8 hours
+sample_time_table_specifications.party_type[0] = 0
+sample_time_table_specifications.party_type[1] = 0
+
+# Time slots
+sample_time_table_specifications.number_of_time_slots = 35  # Assuming 35 time slots
+sample_time_table_specifications.time_slot_ids = [i for i in range(sample_time_table_specifications.number_of_time_slots)]
+sample_time_table_specifications.time_slot_days = [i // 7 for i in range(sample_time_table_specifications.number_of_time_slots)]
+
+#locality table
+sample_time_table_specifications.number_of_localities = 3
+sample_time_table_specifications.locality_ids = [0, 1, 2]  # locality_i = 0, locality_j = 0, locality_i = 0, locality_j = 1, locality_i = 1, locality_j = 1
+sample_time_table_specifications.locality_distances = [0, 10, 0]  # Distances between localities
+
+#venue table
+sample_time_table_specifications.number_of_venues = 15;
+manual_venue_type = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1]
+manual_venue_capacity = [80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 150, 80, 80, 80, 150]
+manual_venue_locality = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
+
+for i in range(sample_time_table_specifications.number_of_venues):
+    sample_time_table_specifications.venue_ids.append(i)
+    sample_time_table_specifications.venue_types.append(manual_venue_type[i])
+    sample_time_table_specifications.venue_capacities.append(manual_venue_capacity[i])
+    sample_time_table_specifications.venue_locality.append(manual_venue_locality[i])
+
+#sessions
+sample_time_table_specifications.number_of_sessions = 30
+manual_s_venue_type = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 2, 2, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0]
+manual_s_duration = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 2, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1]
+manual_s_course = [0, 1, 2, 3, 1, 0, 4, 2, 0, 4, 1, 3, 3, 3, 3, 4, 4, 5, 6, 6, 6, 7, 5, 6, 7, 7, 7, 7, 7, 5]
+
+# Set session data
+for i in range(sample_time_table_specifications.number_of_sessions):
+    sample_time_table_specifications.session_ids(i)
+    sample_time_table_specifications.session_types.append(manual_s_venue_type[i])
+    sample_time_table_specifications.session_durations.append(manual_s_duration[i])
+    sample_time_table_specifications.session_courses.append(manual_s_course[i])   
+
+# Assignments
+sample_time_table_specifications.number_of_assignments = 64
+manual_a_session_id = [0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 11,
+                       12, 12, 12, 13, 13, 13, 14, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 20,
+                       21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29]
+
+manual_a_party_id = [0, 3, 0, 2, 0, 10, 0, 1, 5, 0, 2, 0, 3, 0, 4, 0, 10, 0, 3, 0, 4, 0, 2, 0, 1, 5, 
+                     0, 1, 8, 0, 1, 8, 0, 1, 5, 0, 4, 0, 4, 1, 6, 1, 9, 1, 1, 1, 11, 1, 6, 1, 9, 1, 7, 
+                     1, 7, 1, 7, 1, 7, 1, 7, 1, 6]
+
+# Set assignment data
+for i in range(sample_time_table_specifications.number_of_assignments):
+    sample_time_table_specifications.assignment_party_ids.append(manual_a_party_id[i])
+    sample_time_table_specifications.assignment_session_ids.append(manual_a_session_id[i])
+    sample_time_table_specifications.assignment_priorities.append(0) 
+
