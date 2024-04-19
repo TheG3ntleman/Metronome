6144be24b97a9bcf1571b5aa1eae2f0b870d60ea
TheG3ntleman <sriabhirath21@gmail.com>
2024-04-19 14:39:45 +0530

    Basic Project Structure in Python

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-# Will implment argument parser later
-# for now I'll just hardcode the problem path
+from metronome import TimeTable
 
-from src import *
-
-
-if __name__ == "__main__":
-    sample_specifications = Specifications("data/sample/")
-    [parties, venues, sessions, assignments], conversion_table = sample_specifications.get_numerical_tables()
-    solve_timetable(parties, venues, sessions, assignments)
-
-
+timetable = TimeTable(50)
+timetable.print()
--- src/__init__.py
+++
@@ -1,2 +0,0 @@
-from .utils import *
-from .pmetronome import *
