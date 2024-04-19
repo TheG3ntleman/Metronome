099e952affc8edfa91c4685bc8fbfb1656354f2f
TheG3ntleman <sriabhirath21@gmail.com>
2024-04-19 14:50:16 +0530

    Partially added time table specifications.

--- metronome/common/specifications.py
+++ metronome/common/specifications.py
@@ -1,4 +1,14 @@
 class TimeTableSpecifications:
   
   def __init__(self):
-    pass
+    
+    self.number_of_parties = 0
+    self.party_ids = []
+    self.party_strengths = []
+    self.party_max_hours = []
+    
+    
+    self.number_of_venues = 0
+    self.venue_ids = []
+    self.venue_types = []
+    self.venue_capacities = []
