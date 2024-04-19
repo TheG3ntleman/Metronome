d82bc70aafe8045ea2db7117b040b339ab009b46
HitenSachani <146418778+HitenSachani@users.noreply.github.com>
2024-04-19 15:09:19 +0530

    added specifications to specification.py

--- metronome/common/specifications.py
+++ metronome/common/specifications.py
@@ -6,9 +6,33 @@ class TimeTableSpecifications:
     self.party_ids = []
     self.party_strengths = []
     self.party_max_hours = []
-    
+    self.party_type = []
+    self.party_preffered_start_time = []
+    self.party_preffered_end_time = []
+    self.party_preffered_back_to_back = []
+    self.party_preffered_max_hours = []
     
     self.number_of_venues = 0
     self.venue_ids = []
     self.venue_types = []
     self.venue_capacities = []
+    self.venue_locality = []
+
+    self.number_of_time_slots = 0
+    self.time_slot_ids = []
+    self.time_slot_days = []
+
+    self.number_of_localities = 0
+    self.locality_ids = []
+    self.locality_distances = []
+
+    self.number_of_sessions = 0
+    self.session_ids = []
+    self.session_types = []
+    self.session_durations = []
+    self.session_courses = []
+
+    self.number_of_assignments = 0
+    self.assignment_party_ids = []
+    self.assignment_session_ids = []
+    self.assignment_priorities = []
