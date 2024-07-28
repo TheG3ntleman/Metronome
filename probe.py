aea9b1f80dda5999d87b05ef4793fb52cf319797
CHARANGANESHS <charanganesh10@gmail.com>
2024-07-28 21:27:49 +0530

    Added Solver Probe Analysis class

--- probe.py
+++ probe.py
@@ -9,6 +9,8 @@ from src.solvers.NestedMonteCarlo import NMCTSSolver
 from src.primitives.state_space_tree import StateSpaceTreeNode
 from src.primitives.probe import Probe
 
+from src.probes.SolverProbeAnalysis import SolverAnalysis
+
 import numpy as np # type: ignore
 import random
 
@@ -22,12 +24,24 @@ morpion_solitaire_5d = MorpionSolitaire5D(20)
 
 fmc_solver = FlatMonteCarloSolver()
 mab_solver = UCB1Solver(exploration_parameter=100)
-nmc_solver = NMCTSSolver(depth=10)
+nmc_solver = NMCTSSolver(depth=20)
 
 root_node = StateSpaceTreeNode(None)
-probe = Probe(root_node, nmc_solver, morpion_solitaire_5d, 100)
+# probe = Probe(root_node, nmc_solver, morpion_solitaire_5d, 100)
 
-probe.probe()
-# probe.scatter_plot_rewards()
-probe.kernel_density_plots("results/morpion_solitaire_5d_kde/nmc")
-probe.save("results/morpion_solitaire_5d_kde/nmc")
+# probe.probe()
+# # probe.scatter_plot_rewards()
+# probe.kernel_density_plots("results/morpion_solitaire_5d_kde/nmc")
+# probe.save("results/morpion_solitaire_5d_kde/nmc")
+
+# analysis = SolverAnalysis([fmc_solver, mab_solver, nmc_solver], sudoku_3x3, 10000)
+analysis = SolverAnalysis([fmc_solver, mab_solver, nmc_solver], morpion_solitaire_5d, 5)
+
+
+analysis.perform_analysis()
+
+
+# analysis.kernel_density_plots(save_path = "analysis/sudoku_3x3_kde", show=True)
+analysis.kernel_density_plots(save_path = "analysis/morpion_solitaire_5d/", show=True)
+
+analysis.compare_statistics()
